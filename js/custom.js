ec47dd3b4555b8812eb7c40a1e5f9ac5baaa2e21
Sajid Haider <165992305+sajidhaidersourav@users.noreply.github.com>
2024-10-13 22:58:44 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+// jQuery code here
+$(function () {
+    // preloader start
+    $(window).load(function () {
+      $("#loading").fadeOut(400);
+    });
+    // preloader end
+  });
+
+  // aos animation
+AOS.init({
+    
+});
